d1cb35fde6610293c9885a37b4452399f7d5fb49
chidiebereojingwa <ojingwaoji@gmail.com>
2022-04-10 00:24:30 +0100

    Add toggle function

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -34,7 +34,7 @@ export default function Homepage() {
     }
   return (
     <div>
-      <Exerciseslist exercises={exercises} onDeleteExercise={onDeleteExerciseHandler}/>
+      <Exerciseslist onToggleExercise={toggleExerciseCompletionHandle} exercises={exercises} onDeleteExercise={onDeleteExerciseHandler}/>
     </div>
   )
 }
